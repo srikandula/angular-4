c7cfe98398bfcd5f5ddd071b393fe5eb2cd3ee7b
Srinivas Kandula <srikandula@yahoo.com>
2017-07-02 18:26:08 +0530

    Sample Angular-4 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  pageTitle  = 'Product List';
+  listFilter = 'All';
+  products: any[] = [{
+    'id': 1,
+    'name': 'Bike',
+    'cost': 50,
+    'url': 'http://www.free-icons-download.net/images/bike-icon-77366.png'
+  },
+  {
+    'id': 2,
+    'name': 'Car',
+    'cost': 3000,
+    'url': 'http://www.freeiconspng.com/uploads/vehicle-icon-png-car-sedan-4.png'
+
+  },
+  {
+    'id': 3,
+    'name': 'Mercedes',
+    'cost': 4000,
+    'url': 'https://cdn2.iconfinder.com/data/icons/automobile-repair-service/512/mercedes-car-sedan-auto-front-view-512.png'
+
+  }];
+  constructor() { }
+
+
+
+  ngOnInit() {
+  }
+
+}
